683e51da6a84be2d760676b096cf839b4683dab3
Allison Honold <allisonhonold@gmail.com>
2019-11-12 14:52:25 -0800

    debug model2

--- model2.py
+++ model2.py
@@ -10,14 +10,21 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split, cross_val_score
 from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+import pickle
+
 
 def stamp_time():
+    """stamps the time in the terminal.
+    """
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
 
 
 def print_scores(true, pred, pred_proba):
+    """
+    """
     lloss = log_loss(true, pred_proba)
     cf = confusion_matrix(true, pred)
     acc = accuracy_score(true, pred)
@@ -28,7 +35,7 @@ def print_scores(true, pred, pred_proba):
     
 def categorize_arrests(dataframe):
     cat_df = dataframe.copy()
-    cat_df.loc[cat_df['n_arrests'] > 0] = 1
+    cat_df.loc[cat_df['n_arrests'] > 0, 'n_arrests'] = 1
     cat_df['n_arrests'] = cat_df['n_arrests'].fillna(value=0)
     return cat_df
 
@@ -49,15 +56,36 @@ def split_last(dataframe, sort_col='date', cut=.9):
 def log_loss_cvs(pipe, X_train, y_train):
     ll_scorer = make_scorer(log_loss, greater_is_better=True, needs_proba=True)
 
-    stamp_time()
     scores = cross_val_score(pipe, X_train, y_train, scoring=ll_scorer, cv=5)
-    stamp_time()
     print(scores)
     print(
-        f"95% CI Accuracy: "
+        f"95% CI log loss: "
         f"{round(scores.mean(), 2)} "
         f"(+/- {round(scores.std() * 2, 2)})"
     )
+
+
+def evaluate_model(pipe, X_train, y_train, X_eval, y_eval):
+    print("evaluating model")
+    print("fitting model")
+    pipe.fit(X_train, y_train)
+    print("model fit. Predicting for training set")
+    train_probas = pipe.predict_proba(X_train)
+    print(f"training log loss: {log_loss(y_train, train_probas)}")
+    print("Predicting for evaluation set")
+    test_probas = pipe.predict_proba(X_eval)
+    print(f"test probabilities: {test_probas[0:10]}")
+    test_predict = pipe.predict(X_eval)
+    print(f"test neg log loss: {log_loss(y_eval, test_probas)}")
+    print(f"confusion matrix: \n{confusion_matrix(y_eval, test_predict)}")
+    print(f"accuracy: {accuracy_score(y_eval, test_predict)}")
+    print(f"F1: {f1_score(y_eval, test_predict)}")
+    return test_probas
+
+# def pickle(model, file_name=model.pickle):
+#     output_file = open("walk1.pickle", "wb")
+#     pickle.dump(model, output_file)
+#     output_file.close()
 
 
 def main():
@@ -83,22 +111,22 @@ def main():
         ('ss', StandardScaler(), ['latitude', 'longitude', 'ap_t_high100'])],
         remainder='drop')
 
+    # for max_feature in np.linspace():
+    #     print(f"n_estimators = {n_estimators}")
     rfc = RandomForestClassifier(n_estimators=10, random_state=5, max_depth=10,
-                            class_weight='balanced')
+                            class_weight='balanced', max_features=None)
 
     pipe = Pipeline([
         ('preprocessor', column_transformer),
         ('model', rfc)
     ])
 
-    stamp_time()
-    train_probas = pipe.fit_predict(X_train, y_train)
-    print(f"training neg log loss: {log_loss(y_train, train_probas)}")
-    test_probas = pipe.predict(X_eval)
-    print(f"test neg log loss: {log_loss(y_eval, test_probas)}")
-    stamp_time()
+    # log_loss_cvs(pipe, X_train, y_train)
+
+    evaluate_model(pipe, X_train, y_train, X_eval, y_eval)
 
 
 
-if __name__ == __name__:
+
+if __name__ == "__main__":
     main()
